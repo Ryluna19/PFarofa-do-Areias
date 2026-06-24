63c9e66a6d2888c48adeca8b2e64e516d37ff786
Ryan Santos <Ryluna19gmail.com>
2026-06-24 13:48:49 -0300

    test: configure enviroment variables for CI

---
+++ backend/src/tests/setup.ts
@@ -0,0 +1,6 @@
+process.env.DB_USER = "test_user";
+process.env.DB_HOST = "localhost";
+process.env.DB_NAME = "farofa_test";
+process.env.DB_PASSWORD = "test_password";
+process.env.DB_PORT = "5432";
+process.env.JWT_SECRET = "farofa-test-jwt-secret";
